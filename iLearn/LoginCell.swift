58eb8d919a1f7994ec40f24220f0e27dd21471b8
timothytong <timothy.tong@hotmail.ca>
2015-01-29 15:36:14 -0500

    Custom clear button for textfield, styled for iPad.

--- iLearn/LoginCell.swift
+++ iLearn/LoginCell.swift
@@ -20,9 +20,17 @@ class LoginCell: UITableViewCell{
         input.textColor = UIColor.redColor()
         input.borderStyle = UITextBorderStyle.None
         input.backgroundColor = UIColor(red: 31.0/255.0, green: 31.0/255.0, blue: 31.0/255.0, alpha: 1)
+        var cancelImg = UIImage(named: "cancel-white.png")
+        var clrBtn = UIButton(frame:CGRectMake(0, 0, 26, 30))
+        clrBtn.setImage(cancelImg, forState: .Normal)
+        clrBtn.setImage(cancelImg, forState: .Highlighted)
+        clrBtn.addTarget(self, action: "clearTxt", forControlEvents: .TouchUpInside)
+        input.rightView = clrBtn;
+        input.rightViewMode = .WhileEditing
         var fontSize:CGFloat = 34
         if Constants.is_ipad(){
-            fontSize += 20
+            fontSize += 15
+            label.font = label.font.fontWithSize(50)
         }
         else{
             if Constants.is_iPhone4(){
@@ -33,13 +41,14 @@ class LoginCell: UITableViewCell{
             }
         }
         input.font = UIFont(name: "Avenir Next Condensed", size: fontSize)
-        
-        
     }
     
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
         // Configure the view for the selected state
+    }
+    func clearTxt(){
+        input.text = ""
     }
     
 }
